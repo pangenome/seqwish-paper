150f9e59732cb5c52259bddc8111ddb989e571a7
AndreaGuarracino <andreaguarracino@outlook.com>
2022-01-12 21:33:18 +0100

    add script for logged seqwish

---
+++ scripts/seqwish_with_logging.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ASSEMBLIES=$1
+PAF=$2
+GFA=$3
+k=$4
+B=$5
+LOG=$6
+SECONDS=$7
+
+hostname
+
+cd /scratch
+
+(while true; do (date +%s; du -s .; ls -s; echo) >>"$LOG"; sleep "$SECONDS"; done) &
+SIZE_DEMON_PID=$!
+
+\time -v ~/tools/seqwish/bin/seqwish-ccfefb016fcfc9937817ce61dc06bbcf382be75e -t 48 -s "$ASSEMBLIES" -p "$PAF" -g "$GFA" -k "$k" -B "$B" -P
+
+kill $SIZE_DEMON_PID
